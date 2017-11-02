8c937da734b5c959e95e7862ff1418f8c15b8324
Siarhei Rylach <32290787+SiarheiRylach@users.noreply.github.com>
2017-11-02 10:52:22 +0300

    Update main.page.js

--- task2/pages/main.page.js
+++ task2/pages/main.page.js
@@ -12,8 +12,6 @@ class MainPage {
         this.logInBtn = element(by.linkText("LOG IN"));
         this.signUpBtn = element(by.linkText("SIGN UP"));
         this.createAccountBtn = element(by.linkText("Create Account"));
-        this.logInBtn = element(by.linkText("LOG IN"));
-        this.signUpBtn = element(by.linkText("SIGN UP"));
         this.getAndroidApp = element(by.xpath('//span[text()="Get the Android App"]/parent::a'));
         this.getIOSApp = element(by.xpath('//span[text()="Get the iOS App"]/parent::a'));
         this.useWebVersionBtn = element(by.xpath('//span[text()="Use the Web Version"]/parent::a'));
